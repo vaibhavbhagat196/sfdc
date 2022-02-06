c31ef99b6d795978cb3bf2429a2c688ad216ded7
vaibhavbhagat196 <99129066+vaibhavbhagat196@users.noreply.github.com>
2022-02-07 01:03:46 +0530

    add

---
+++ swap.java
@@ -0,0 +1,18 @@
+package com.capgemini.sfdc;
+
+public class swap {
+	
+		public static void main(String[]args) {
+
+		int a=10;
+		int b=20;
+		
+		 System.out.println("Before Swaping a= "+a+"b="+b);
+		 
+		 int temp=a;
+		 a=b;
+		 b=temp;
+		 System.out.println("After  Swaping a= "+a+"b="+b);
+		}		
+
+}
