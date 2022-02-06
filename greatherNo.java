e0b60fc97bff18006bfe07feaa180199000b7199
vaibhavbhagat196 <99129066+vaibhavbhagat196@users.noreply.github.com>
2022-02-07 00:57:54 +0530

    add

---
+++ greatherNo.java
@@ -0,0 +1,11 @@
+package com.capgemini.sfdc;
+
+public class greatherNo {
+	public static void main(String[]args) {
+
+	int a=10;
+	int b=20;
+	 int result=(a>b)?a:b;
+	 System.out.println("largest number is "+result);
+	}		
+}
