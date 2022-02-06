2bb3dc1072c70304f9cd22e38f86f4acabf358d5
vaibhavbhagat196 <99129066+vaibhavbhagat196@users.noreply.github.com>
2022-02-07 00:47:14 +0530

    add

--- sum1.java
+++ sum1.java
@@ -1,21 +1,23 @@
 package com.capgemini.sfdc;
+//  question n0.c10 sum of number 100 and less than 200 
 import java.util.Scanner;
-
 public class sum1 {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("enter the number");
-  int sum=0;
-	int num=sc.nextInt();
-	while(num>100)
-	{
-		if(num%7==0) {
-			sum=sum+num;
-			
+	public static void main(String[] args) 
+	{ 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int num=sc.nextInt();
+		int sum=0;
+		while(num<200)//1234>0 true
+		{
+			if(num%7==0) {
+				sum=sum+num;
+			}
+			num++;
 		}
-		else {
-			System.out.println("please enter valid number");
-		}
+		System.out.println("sum of the number divisible by 7 is "+sum);
+
 	}
-	
+
 
 }
