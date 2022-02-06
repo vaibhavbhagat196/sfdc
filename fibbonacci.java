f6cf944a70120c6dc0e9569f425c142a2aa78d4b
vaibhavbhagat196 <99129066+vaibhavbhagat196@users.noreply.github.com>
2022-02-07 00:33:45 +0530

    add

---
+++ fibbonacci.java
@@ -0,0 +1,26 @@
+package com.capgemini.sfdc;
+import java.util.Scanner;
+public class fibbonacci {
+	public static void main(String[] args) 
+	{  
+		int a=0,b=1;
+		int c=0;
+        Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the start value");
+		int num=sc.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		while(num>=1)//5>=1  4>=1; 3>=1
+		{
+			c=a+b;//c=1; 2; 3; 
+			System.out.println(c);//1 2 
+			a=b;//1
+			b=c;//1 2 3
+			num--;//5-----4----3
+			
+		}
+
+
+	}
+
+}
